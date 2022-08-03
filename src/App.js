86772d369d4005da915bcf5cbe99b26a16aa24e2
rbrt1022 <rbrt1022.it@outlook.hu>
2022-08-03 20:41:55 +0200

    ProbaSzerencse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  HashRouter,
+  matchRoutes,
+  useRoutes
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<BrowserRouter>
+      <HashRouter basename="/">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <Routes> */}
+        <useRoutes>
+          {/* <Route path="/react-router-test/about">
+            <About />
+          </Route>
+          <Route path="/react-router-test/users">
+            <Users />
+          </Route>
+          <Route path="/react-router-test/">
+            <Home />
+          </Route> */}
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/users" element={<Users />} />
+            
+          </Routes>
+        {/* </Routes> */}
+        </useRoutes>
+      </HashRouter>
+    //</BrowserRouter>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
